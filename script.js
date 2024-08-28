a2f67be60106e493e1028a0bf29a666c993872c7
Ifram Lopes <160543554+IfraimLopes@users.noreply.github.com>
2024-08-28 22:37:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+function pink1() {
+  let pink = window.document.querySelector(".nikejordan");
+  let txt = window.document.querySelector(".txt");
+  let body = window.document.querySelector("body");
+  let pa = window.document.querySelector(".pa");
+  pink.src = "img/07Wwxj13_c2EstQlCJ8s0-transformed.webp";
+  body.style.background = "pink";
+  txt.innerHTML = `LeBron XXI "Dragon Pearl"`;
+  pa.innerHTML = `O LeBron XXI "Dragon Pearl" é um modelo de tênis da Nike  <br /> que faz parte da linha de calçados de basquete  <br /> assinada por LeBron James, um dos maiores   <br />jogadores da NBA.`;
+}
+
+function pink() {
+  let txt = window.document.querySelector(".txt");
+  let pa = window.document.querySelector(".pa");
+  let pink = window.document.querySelector(".nikejordan");
+  let body = window.document.querySelector("body");
+  pink.src = "img/Remove-bg.ai_1724536049259.png";
+  txt.innerHTML = `NIKE Air Jordan`;
+  body.style.background = "red";
+  a.innerHTML = `Os Air Jordans são uma das linhas de tênis mais <br />
+          icônicas e influentes no mundo do esporte e da moda. <br />Criados em
+          parceria entre Michael Jordan, <br />
+          um dos maiores jogadores de basquete de todos os tempos.`;
+}
+
+function pink3() {
+  let txt = window.document.querySelector(".txt");
+  let pa = window.document.querySelector(".pa");
+  let pink = window.document.querySelector(".nikejordan");
+  let body = window.document.querySelector("body");
+  pink.src = "img/Remove-bg.ai_1724535970025.png";
+  body.style.background = "rgb(4, 92, 4)";
+  txt.innerHTML = `NIKE Air Jordan`;
+  a.innerHTML = `Os Air Jordans são uma das linhas de tênis mais <br />
+          icônicas e influentes no mundo do esporte e da moda. <br />Criados em
+          parceria entre Michael Jordan, <br />
+          um dos maiores jogadores de basquete de todos os tempos.`;
+}
+
+function pink4() {
+  let txt = window.document.querySelector(".txt");
+  let pa = window.document.querySelector(".pa");
+  let pink = window.document.querySelector(".nikejordan");
+  let body = window.document.querySelector("body");
+  pink.src = "img/WMNS+AIR+JORDAN+1+LOW-removebg-preview.png";
+  body.style.background = "skyblue";
+  txt.innerHTML = `Air Jordan 1 Low`;
+  a.innerHTML = `O Air Jordan 1 Low é uma variação do icônico Air Jordan 1<br /> , que é um dos modelos de tênis <br /> mais emblemáticos da Nike. `;
+}
